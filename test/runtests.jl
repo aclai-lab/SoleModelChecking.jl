2a9848e4bd951031f981f9b99f62bfbf8d4c3731
paraandrea <andrea.paradiso.uni@gmail.com>
2022-07-08 12:51:51 +0200

    add a new test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using SoleModelChecking
 using Test
 
 @testset "SoleModelChecking.jl" begin
+
     #=
         the idea bheind the testing is the following:
             - testing if the expression without parenthesis is equal length of the one
@@ -10,25 +11,52 @@ using Test
             - testing if the combination of the symbols are int the expression
     =#
 
-    expression = "(¬(a∧b)∨(□c∧◊d))"
+    @testset "Fisrt expression check" begin
+        expression = "(¬(a∧b)∨(□c∧◊d))"
 
-    #testing length of the formulas without parenthesis
-    expressionNoPar = replace(expression,['(',')'] => "");
-    resultExpr = shunting_yard(expression)
-    @test length(resultExpr) == length(expressionNoPar)
+        #testing length of the formulas without parenthesis
+        expressionNoPar = replace(expression,['(',')'] => "");
+        resultExpr = shunting_yard(expression)
+        @test length(resultExpr) == length(expressionNoPar)
 
-    #testing if all the symbol obtained by shouting_yard are in the expression
-    for symbol in resultExpr
-        @test occursin(symbol,expression)
+        #testing if all the symbol obtained by shouting_yard are in the expression
+        for symbol in resultExpr
+            @test occursin(symbol,expression)
+        end
+
+        formula = tree(resultExpr)
+
+        #testing if all the subformulas are contained in the expression
+        subf_array = String[]
+        φ = subformulas(formula.tree, subf_array)
+        for subformula in φ
+            @test occursin(subformula,expression)
+        end
     end
 
-    formula = tree(resultExpr)
+    @testset "Second expression check" begin
+        expression = "((((((((((((a))))))))))))"
 
-    #testing if all the subformulas are contained in the expression
-    subf_array = String[]
-    φ = subformulas(formula.tree, subf_array)
-    for subformula in φ
-        @test occursin(subformula,expression)
+        #testing length of the formulas without parenthesis
+        expressionNoPar = replace(expression,['(',')'] => "");
+        resultExpr = shunting_yard(expression)
+        @test length(resultExpr) == length(expressionNoPar)
+
+        #testing if all the symbol obtained by shouting_yard are in the expression
+        for symbol in resultExpr
+            @test occursin(symbol,expression)
+        end
+
+        formula = tree(resultExpr)
+
+        #testing if all the subformulas are contained in the expression
+        subf_array = String[]
+        φ = subformulas(formula.tree, subf_array)
+        for subformula in φ
+            @test occursin(subformula,expression)
+        end
     end
+
+    #testing some operators here
 
 end
