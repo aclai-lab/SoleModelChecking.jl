7d13fdd0cd9bce30425b9ac54ef1922a82f57c71
paraandrea <andrea.paradiso.uni@gmail.com>
2022-07-08 12:55:51 +0200

    add a new test

--- src/SoleModelChecking.jl
+++ src/SoleModelChecking.jl
@@ -6,9 +6,6 @@ include("parser.jl")
 include("formula_tree.jl")
 include("checker.jl")
 
-export shunting_yard;
-export tree;
-export inorder
-export subformulas
+export shunting_yard, tree, inorder, subformulas
 
 end
