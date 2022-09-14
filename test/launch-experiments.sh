1f95ab9c5508e1977e371de3382e21e22e226bb0
mauro-milella <milella_mauro@live.it>
2022-09-15 01:34:16 +0200

    Launch experiments extension

--- test/launch-experiments.sh
+++ test/launch-experiments.sh
@@ -6,7 +6,8 @@ exec_n_worlds_per_model=(20)
 exec_n_letters=(2)
 exec_n_formulas=(1000)
 exec_max_formula_height=(3)
-exec_n_threads=(1 2)
+exec_pruning_factor=(0.1 0.5 0.7 1)
+exec_n_threads=(1)
 
 for repetitions in "${exec_repetitions[@]}"; do
 for n_models in "${exec_n_models[@]}"; do
@@ -14,8 +15,11 @@ for n_worlds_per_model in "${exec_n_worlds_per_model[@]}"; do
 for n_letters in "${exec_n_letters[@]}"; do
 for n_formulas in "${exec_n_formulas[@]}"; do
 for max_formula_height in "${exec_max_formula_height[@]}"; do
+for pruning_factor in "${exec_pruning_factor[@]}"; do
 for n_threads in "${exec_n_threads[@]}"; do
-	julia -t$n_threads --project=. test/experiments.jl $n_models $n_worlds_per_model $n_letters $max_formula_height $n_formulas $repetitions
+	echo MODELS: $exec_n_models NLETTERS: $n_letters MAXHEIGHT: $max_formula_height NFORMULAS: $n_formulas PRFACTOR: $pruning_factor REPS: $repetitions
+	julia -t$n_threads --project=. test/experiments.jl --nmodels $n_models --nworlds $n_worlds_per_model --nletters $n_letters --fmaxheight $max_formula_height --nformulas $n_formulas --prfactor $pruning_factor --nreps $repetitions
+done
 done
 done
 done
