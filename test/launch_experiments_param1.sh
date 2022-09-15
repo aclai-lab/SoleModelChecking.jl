0ca329a680d71492ef398a625ea9e51836984e8d
mauro-milella <milella_mauro@live.it>
2022-09-15 11:21:20 +0200

    Experiments computational load splitted in 6 files

---
+++ test/launch_experiments_param1.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+exec_repetitions=(10000)
+exec_pruning_factor=(0.7)
+exec_n_models=(10)
+exec_n_worlds_per_model=(10)
+exec_n_letters=(2 8)
+exec_max_formula_height=(1 2 4 8)
+exec_n_formulas=(5000)
+exec_n_threads=(1)
+
+for repetitions in "${exec_repetitions[@]}"; do
+for pruning_factor in "${exec_pruning_factor[@]}"; do
+for n_models in "${exec_n_models[@]}"; do
+for n_worlds_per_model in "${exec_n_worlds_per_model[@]}"; do
+for n_letters in "${exec_n_letters[@]}"; do
+for n_formulas in "${exec_n_formulas[@]}"; do
+for max_formula_height in "${exec_max_formula_height[@]}"; do
+for n_threads in "${exec_n_threads[@]}"; do
+	echo MODELS: $n_models NLETTERS: $n_letters MAXHEIGHT: $max_formula_height NFORMULAS: $n_formulas PRFACTOR: $pruning_factor REPS: $repetitions
+	julia -t$n_threads --project=. test/experiments.jl --nmodels $n_models --nworlds $n_worlds_per_model --nletters $n_letters --fmaxheight $max_formula_height --nformulas $n_formulas --prfactor $pruning_factor --nreps $repetitions
+done
+done
+done
+done
+done
+done
+done
+done
