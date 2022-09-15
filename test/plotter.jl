36e92c8a2c5c47e4388ff280582448d3a03511dd
mauro-milella <milella_mauro@live.it>
2022-09-15 23:15:17 +0200

    New launchers

--- test/plotter.jl
+++ test/plotter.jl
@@ -1,12 +1,53 @@
-using Plots
+using Plots, Plots.Measures
 using ArgParse
 using CSV
+# using DelimitedFiles
+using Tables
 
-# Given a csv about a Multiple Models Multiple Formulas Model Checking
-# (see experiments.jl), plot it in a clean fashion way.
-function plot_mmcheck(args)
-    @show typeof(args)
-    csv_reader = CSV.File(args["file"])
+#=
+Attributes
+https://docs.juliaplots.org/latest/generated/attributes_series/
+=#
+
+# Utility script to reproduce specific plots from csv file.
+
+function plot_mmcheck(args::Dict{String, Any})
+    times = CSV.read(args["file"], Tables.matrix, header=0)
+    times2 = times .* 2
+
+    nrows, ncols = Base.size(times)
+
+    # Simple plot
+    # NOTE: different pruning factor could be plotted using different
+    # linestyles (e.g. linestyle=:dash or :dashdot)
+    plt1 = plot()
+    for row in 1:nrows
+        plot!(
+            plt1,
+            1:ncols,
+            cumsum(times[row,:]),
+            labels="$row",
+            linestyle=:solid,
+            margins=15mm
+        )
+    end
+    display(plt1)
+
+    # Scatter plot
+    plt2 = plot()
+    for row in 1:nrows
+        scatter!(
+            plt2,
+            1:ncols,
+            times[row,:],
+            labels="$row",
+            linestyle=:solid,
+            margins=15mm,
+            markersize=2,
+            markerstrokewidth=0
+        )
+    end
+    display(plt2)
 end
 
 function parse_commandline()
