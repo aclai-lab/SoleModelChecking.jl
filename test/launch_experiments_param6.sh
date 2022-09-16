ece308fb3450f57e386dcd9c1ab4f896553c73cd
mauro-milella <milella_mauro@live.it>
2022-09-16 11:10:32 +0200

    New set of launchers

--- test/launch_experiments_param6.sh
+++ test/launch_experiments_param6.sh
@@ -2,20 +2,20 @@
 #
 exec_repetitions=(1000)
 exec_pruning_factor=(0.8)
-exec_n_models=(10)
-exec_n_worlds_per_model=(10)
-exec_n_letters=(16)
+exec_n_models=(50)
+exec_n_worlds_per_model=(20)
+exec_n_letters=(2 4 8 16)
 exec_max_formula_height=(8)
 exec_n_formulas=(8000)
 exec_n_threads=(1)
 
 for repetitions in "${exec_repetitions[@]}"; do
-for pruning_factor in "${exec_pruning_factor[@]}"; do
-for n_models in "${exec_n_models[@]}"; do
-for n_worlds_per_model in "${exec_n_worlds_per_model[@]}"; do
-for n_letters in "${exec_n_letters[@]}"; do
 for n_formulas in "${exec_n_formulas[@]}"; do
 for max_formula_height in "${exec_max_formula_height[@]}"; do
+for pruning_factor in "${exec_pruning_factor[@]}"; do
+for n_letters in "${exec_n_letters[@]}"; do
+for n_models in "${exec_n_models[@]}"; do
+for n_worlds_per_model in "${exec_n_worlds_per_model[@]}"; do
 for n_threads in "${exec_n_threads[@]}"; do
 	echo MODELS: $n_models NLETTERS: $n_letters MAXHEIGHT: $max_formula_height NFORMULAS: $n_formulas PRFACTOR: $pruning_factor REPS: $repetitions
 	julia -t$n_threads --project=. test/experiments.jl --nmodels $n_models --nworlds $n_worlds_per_model --nletters $n_letters --fmaxheight $max_formula_height --nformulas $n_formulas --prfactor $pruning_factor --nreps $repetitions
