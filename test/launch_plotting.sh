8abf278518fce9f0ba5177e175dc9b68e7b3e0c5
mauro-milella <milella_mauro@live.it>
2022-09-18 17:57:58 +0200

    plotter utility

---
+++ test/launch_plotting.sh
@@ -0,0 +1,19 @@
+exec_n_letters=(2 4 8 16)
+exec_max_formula_height=(2 4)
+n_models=10
+n_worlds=10
+for n_letters in "${exec_n_letters[@]}"; do
+for fheight in "${exec_max_formula_height[@]}"; do
+julia --project=. test/plotter.jl --directory=test/csv/simple_$(n_models)_$(n_letters)_$(fheight) --title="#Models=$(n_models), #Worlds=$(n_worlds), #Letters=$(n_letters), MaxHeight=$(fheight)" --memolabel="no 0 1 2 3 4 8" --prlabel="0.2 0.5 0.8"
+done
+done
+
+exec_n_letters=(2 4 8 16)
+exec_max_formula_height=(8)
+n_models=10
+n_worlds=10
+for n_letters in "${exec_n_letters[@]}"; do
+for fheight in "${exec_max_formula_height[@]}"; do
+julia --project=. test/plotter.jl --directory=test/csv/simple_$(n_models)_$(n_letters)_$(fheight) --title="#Models=$(n_models), #Worlds=$(n_worlds), #Letters=$(n_letters), MaxHeight=$(fheight)" --memolabel="no 0 1 2 3 4 8" --prlabel="0.2 0.5 0.8"
+done
+done
--- test/plotter.jl
+++ test/plotter.jl
@@ -1,38 +1,74 @@
 using Plots, Plots.Measures
+# using PGFPlots
 using ArgParse
 using CSV
-# using DelimitedFiles
 using Tables
 
 #=
-Attributes
+Plot attributes
 https://docs.juliaplots.org/latest/generated/attributes_series/
 =#
 
-# Utility script to reproduce specific plots from csv file.
+function check_extension(filename::String)
+    filename[findlast(isequal('.'), filename):end]
+end
 
+# Utility script to reproduce specific plots from the csv files
+# contained in a directory.
 function plot_mmcheck(args::Dict{String, Any})
-    times = CSV.read(args["file"], Tables.matrix, header=0)
-    times2 = times .* 2
+    files = []
+    for file in readdir(args["directory"])
+        if check_extension(file) != ".csv"
+            continue
+        end
 
-    nrows, ncols = Base.size(times)
+        filepath = joinpath(pwd(), args["directory"], file)
+        push!(files, CSV.read(filepath, Tables.matrix, header=0))
+    end
 
-    # Simple plot
-    # NOTE: different pruning factor could be plotted using different
-    # linestyles (e.g. linestyle=:dash or :dashdot)
+    # Exported plot name
+    fname = split(args["directory"], Base.Filesystem.path_separator)[end]
+
+    # Collections to make plot meaningfull
+    theme(:vibrant)
+    lcolors = theme_palette(:vibrant)
+    lstyles = [:solid :dash :dot :dashdot :dashdotdot]
+    memo_label = [split(args["memolabel"])...]
+    prf_label = [split(args["prlabel"])...]
+
     plt1 = plot()
-    for row in 1:nrows
-        plot!(
-            plt1,
-            1:ncols,
-            cumsum(times[row,:]),
-            labels="$row",
-            linestyle=:solid,
-            margins=15mm
-        )
+    for i in eachindex(files)
+        theme(:vibrant)
+        nrows, ncols = Base.size(files[i])
+
+        for row in 1:nrows
+
+            #NOTE: this will be removed, it's only purpose is to remove memo3 from plots
+            if nrows >= 6 && row == 5 continue end
+
+            plot!(
+                plt1,
+                cumsum(files[i][row,:]),
+                linestyle=lstyles[i],
+                linecolor=lcolors[row],
+                title=args["title"],
+                legend=:topleft,
+                label="memo: $(memo_label[row]), pr: $(prf_label[i])",
+                margins=5mm
+            )
+        end
     end
     display(plt1)
+    savefig(plt1, joinpath(pwd(), "test", "plots", "$(fname).png"))
 
+    #=
+    linestyle=lstyles[i],
+    labels="$row",
+    legend=:topleft,
+    margins=15mm
+    =#
+
+#=
     # Scatter plot
     plt2 = plot()
     for row in 1:nrows
@@ -48,14 +84,25 @@ function plot_mmcheck(args::Dict{String, Any})
         )
     end
     display(plt2)
+    =#
+end
+
+function ArgParse.parse_item(::Type{Vector{String}}, x::AbstractString)
+    return [split(x)]
 end
 
 function parse_commandline()
     s = ArgParseSettings()
 
     @add_arg_table s begin
-        "--file"
-            help = "Number of kripke models"
+        "--directory"
+            help = "Directory containing all the CSVs to plot"
+        "--title"
+            help = "Plot title"
+        "--memolabel"
+            help = "Labels regarding memoization"
+        "--prlabel"
+            help = "Labels regarding pruning factor labels"
     end
 
     return parse_args(s)
